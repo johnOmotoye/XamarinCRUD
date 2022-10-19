25e5d089776bff1522f610189059753668af7373
Drpampam <john.omotoye@decagon.dev>
2022-10-19 05:17:38 +0100

    first commit

---
+++ StudentCRUDApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using StudentCRUDApp.Services;
+using StudentCRUDApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudentCRUDApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ StudentCRUDApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StudentCRUDApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StudentCRUDApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
